6f6aae73ed2ba948e432a402c917e4cb1d96fb56
Chala Geta <chalageta21@gmail.com>
2024-08-26 16:36:35 +0300

    End

--- src/Components/Profile/User.jsx
+++ src/Components/Profile/User.jsx
@@ -1,12 +1,17 @@
 import userLogo from "../../assets/user01.png";
 const User = () => {
   return (
-      <div className="flex gap-3 items-center bg-white p-4 rounded-full">
-          <img src={userLogo} alt="" className="w-14 h-14 rounded-full" />
-          <div><h3>Chala Geta</h3>
-          <p>Developer</p></div>
+    <div
+      className="flex gap-3 items-center bg-white p-4 rounded-full dark:bg-gray-600
+     dark:text-gray-300"
+    >
+      <img src={userLogo} alt="" className="w-14 h-14 rounded-full" />
+      <div>
+        <h3 className="font-semibold text-2xl">Chala Geta</h3>
+        <p>Developer</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
